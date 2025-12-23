a475556e384f4791c4f17056c55827a765e465b6
Damar <damar728@gmail.com>
2025-12-23 10:56:46 +0700

    sitemap

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-  const sitemapUrl = "https://ahhhiqcnnwpfbgdggvct.supabase.co/storage/v1/object/public/seo/sitemap.xml";
+  const response = await fetch(
+    "https://ahhhiqcnnwpbfqdggvct.supabase.co/functions/v1/sitemap"
+  );
 
-  const response = await fetch(sitemapUrl);
   const xml = await response.text();
 
   res.setHeader("Content-Type", "application/xml");
