2563df7d7679b920078f0eb26bd022022cf8e18d
Damar <damar728@gmail.com>
2026-01-25 17:40:43 +0700

    fix

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -1,9 +1,12 @@
 export default async function handler(req, res) {
-  const response = await fetch(
-    "https://ahhhiqcnnwpfbgdggvct.supabase.co/functions/v1/sitemap"
+  const r = await fetch(
+    "https://ahhhiqcnnwpfbgdggvct.supabase.co/functions/v1/sitemap",
+    { cache: "no-store" }
   );
-  const xml = await response.text();
 
-  res.setHeader("Content-Type", "application/xml");
+  const xml = await r.text();
+
+  res.setHeader("Content-Type", "application/xml; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store, max-age=0");
   res.status(200).send(xml);
 }
